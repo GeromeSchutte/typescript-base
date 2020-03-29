421f6c6f0ef78fb48e8d1ce90291c5479e745a87
Gerome <gerome.schutte@hotmail.com>
2020-03-29 18:59:32 +0200

    Add sass support with live reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import './App.scss';
 
 const App: React.FunctionComponent = () => {
-    return <><span>TypeScript Base with React, Webpack, Jest and Live Reloading 🙌</span></>;
+    return <><span className="font">TypeScript Base with React, Webpack, Jest and Live Reloading 🙌 and other stuff!</span></>;
 };
 
 export default hot(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 const enableModuleOutput = {
     library: 'typescriptBase',
@@ -9,7 +14,7 @@ const enableModuleOutput = {
 
 module.exports = {
     entry: './src/index.tsx',
-    devtool: 'inline-source-map',
+    devtool: isDevelopment ? 'inline-source-map' : 'none',
     module: {
         rules: [
             {
@@ -19,6 +24,14 @@ module.exports = {
                 },
                 exclude: /node_modules/,
             },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
         ],
     },
     resolve: {
@@ -33,5 +46,11 @@ module.exports = {
         ...enableModuleOutput,
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [new ForkTsCheckerWebpackPlugin(), new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public/index.html')})]
+    plugins: [
+        //new CleanWebpackPlugin(['dist']),
+        new ForkTsCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html')
+        })
+    ]
 };
